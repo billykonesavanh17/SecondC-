505e61e15527c04b4f828d9a20e06ee5729659bb
Billy Konesavanh <billykonesavanh17@gmail.com>
2016-01-19 09:46:29 -0700

    Added to repo

---
+++ src/Controller/Runner.cpp
@@ -0,0 +1,16 @@
+/*
+ * Runner.cpp
+ *
+ *  Created on: Jan 19, 2016
+ *      Author: bkon4208
+ */
+
+#include "SillyAppController.h"
+
+int main()
+{
+	SillyAppController * baseController = new SillyAppController();
+	baseController -> start();
+	return 0;
+}
+
